b8a9052f87b2da16ff33513e19b5de6364da396d
Alfredosang <osangalfred@yahoo.com>
2015-05-20 19:59:43 +0100

    Alfred-C-assignment

---
+++ number 3.cpp
@@ -0,0 +1,23 @@
+/*
+* number3.cpp
+* Name: [Osang Bosa-osowo Alfred]
+* Department: [Computer Science]
+* Matric No: [46033051DI]
+* A Program that print a box,oval,arrow and a diamond 
+*
+* Stub file to enable you complete assignment#1-question#3
+*/
+#include <iostream>
+
+int main(){
+	std::cout << "\n*********        ***           *             *";
+	std::cout << "\n*       *     *       *       ***           * *";
+	std::cout << "\n*       *    *         *     *****         *   *";
+	std::cout << "\n*       *    *         *       *          *     *";
+	std::cout << "\n*       *    *         *       *         *       *";
+	std::cout << "\n*       *    *         *       *          *     *";
+	std::cout << "\n*       *    *         *       *           *   *";
+	std::cout << "\n*       *     *       *        *            * *";
+	std::cout << "\n*********        ***           *             *";
+}
+
